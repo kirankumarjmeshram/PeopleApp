c899eee8ecba1a6d12b63b8d4ab6b40604eafbbb
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-04-13 14:24:16 +0530

    work on signPromice

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,22 +1,62 @@
 require("dotenv").config();
 const API_URL = process.env.API_URL;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(API_URL);
 const contract = require("../artifacts/contracts/PeopleApp.sol/PeopleApp.json");// calling MyNFT json 
-const contractAddress = "0xc8C5abcb57056098c07788e1bA439CCcb499bBDB";
+const contractAddress = "0xe16C043F7a38DC6456717C63a8f07fb273D937a2";
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress); //Creating instance of contract
 //create transaction
+//
 async function addPerson(name, age, myNumber, myAddress) {
-  nftContract.methods.addPerson(name, age, myNumber, myAddress).call((error, result)=>{
-    if(error){
-      console.log(error);
-    }else{
-      console.log("Person detail is added")
-    }
-  });
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
+
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.addPerson(name, age, myNumber, myAddress).encodeABI(),
+  };
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
 
 }
+//
+// async function addPerson(name, age, myNumber, myAddress) {
+//   nftContract.methods.addPerson(name, age, myNumber, myAddress).call((error, result)=>{
+//     if(error){
+//       console.log(error);
+//     }else{
+//       console.log("Person detail is added")
+//     }
+//   });
+// }
 async function getPersonDetail(myAddress) {
   nftContract.methods.getPersonDetail(myAddress).call((error, result)=>{
     if(error){
@@ -39,6 +79,8 @@ async function updateNumber(myAddress, newMyNumber) {
   });
 }
 
-addPerson("name3",23,223,"myAddress3");
-//getPersonDetail("myAddress");
+
+
+//addPerson("name1",11,111,"myAddress1");
+getPersonDetail("myAddress1");
 //updateNumber("myAddress2", 33)
